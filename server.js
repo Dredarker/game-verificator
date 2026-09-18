79c13c4a4f0fbfe1cf22e7393d74e1004df45209
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-09-18 16:15:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 
 const PORT = process.env.PORT || 3000;
+const STORAGEAPI = process.env.storageAPI;
 
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -10,9 +11,7 @@ const server = http.createServer((req, res) => {
   if (req.url === "/healthz") {res.writeHead(200, {"Content-Type": "text/plain"}); return res.end("200 OK");}
   else {
     const url = new URL(req.url, `https://${req.headers.host || "localhost"}`);
-    const body = [...url.searchParams.entries()]
-      .map(([key, value]) => `${key}=${value}`)
-      .join("\n");
+    console.log(url.searchParams);
 
     res.writeHead(200, {
       "Content-Type": "text/plain; charset=utf-8"
@@ -24,3 +23,14 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`HTTP server listening on port ${PORT}`);
 });
+
+function isGame(key, value, value2) {
+  return key === "game" && value === value2
+}
+
+async function getInStorage(key) {
+  fetch("https://ikelene.net/storage/get.php", {
+    method: 'POST',
+    body: {apiKey: STORAGEAPI, key}
+  })
+}
