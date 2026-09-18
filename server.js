c1b3e2c942f34fde4d855c3c460153e3814091d6
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-09-18 16:15:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "Content-Type": "text/plain; charset=utf-8"
     });
-    res.end(body);
+    res.end("got");
   }
 });
 
