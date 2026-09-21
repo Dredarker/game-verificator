5c9983883eca2950c0aa6fe3aa94f53f30d08984
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-09-22 01:52:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ server.listen(PORT, () => {
 });
 
 async function getInStorage(key) {
+  console.log(STORAGEAPI+"; "+key)
   let response = await fetch("https://ikelene.net/storage/get.php", {
     method: 'POST',
     body: {apiKey: STORAGEAPI, key}
